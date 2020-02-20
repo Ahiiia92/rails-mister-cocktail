15d662f58aea36380b7cac35da0bc5dad1f53240
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-02-20 12:18:09 +0100

    3 Models Specs DONE: Validation, associations and attributes. Status: DONE

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,9 +3,8 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, presence: true
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
   validates :ingredient, presence: true
-  validates :[cocktail, ingredient], uniqueness: true
 
  ## A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :dose
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
