6f4e4327509501fbbc411d64d11f53fb103e87f2
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-02-24 23:38:43 +0100

    TODO: Edit Doses to add

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,6 +16,19 @@ def create
   end
 end
 
+def edit
+  set_dose
+end
+
+def update
+  set_dose
+  if @dose.update(dose_params)
+    rediret_to cocktail_path(@cocktail), notice: 'Recipe was successfully updated.'
+  else
+    render :edit
+  end
+end
+
 def destroy
   @dose = Dose.find(params[:cocktail_id])
   rediret_to cocktail_path(@cocktail)
@@ -25,5 +38,9 @@ private
 
 def dose_params
   params.require(:dose).permit(:description, :ingredient_id)
+end
+
+def set_dose
+  @dose = Dose.find(params[:cocktail_id])
 end
 end
