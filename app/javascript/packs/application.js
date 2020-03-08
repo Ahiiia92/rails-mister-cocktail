e9d183ad81bdb27979d21d800ff9963550c63aae
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-03-08 22:26:42 +0100

    tried out some javascripts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+console.log("Hello from app/javascript/packs/application.js!");
+
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
