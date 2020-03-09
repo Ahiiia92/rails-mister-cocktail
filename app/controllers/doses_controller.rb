a444327e329200de2cb5e133f957e1ccbdebb48b
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-03-09 21:45:10 +0100

    AJAX created for create, TODO: update DESTROY

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,46 +1,52 @@
 class DosesController < ApplicationController
 
-def new
-  @cocktail = Cocktail.find(params[:cocktail_id])
-  @dose = Dose.new
-end
-
-def create
-  @cocktail = Cocktail.find(params[:cocktail_id])
-  @dose = Dose.new(dose_params)
-  @dose.cocktail = @cocktail
-  if @dose.save
-    redirect_to cocktail_path(@cocktail)
-  else
-    render :new
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
-end
 
-def edit
-  set_dose
-end
-
-def update
-  set_dose
-  if @dose.update(dose_params)
-    rediret_to cocktail_path(@cocktail), notice: 'Recipe was successfully updated.'
-  else
-    render :edit
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      respond_to do |format|
+          format.html { redirect_to cocktail_path(@cocktail) }
+          format.js  # <-- will render `app/views/reviews/create.js.erb`
+        end
+    else
+    respond_to do |format|
+      format.html { render :new }
+      format.js  # <-- idem
+      end
+    end
   end
-end
 
-def destroy
-  @dose = Dose.find(params[:cocktail_id])
-  rediret_to cocktail_path(@cocktail)
-end
+  def edit
+    set_dose
+  end
 
-private
+  def update
+    set_dose
+    if @dose.update(dose_params)
+      rediret_to cocktail_path(@cocktail), notice: 'Recipe was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
-def dose_params
-  params.require(:dose).permit(:description, :ingredient_id)
-end
+  def destroy
+    @dose = Dose.find(params[:cocktail_id])
+    rediret_to cocktail_path(@cocktail)
+  end
 
-def set_dose
-  @dose = Dose.find(params[:cocktail_id])
-end
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:cocktail_id])
+  end
 end
