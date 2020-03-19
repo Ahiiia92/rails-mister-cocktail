ffc3c5cf7774d92012e5522741959c13dcfd1441
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-03-19 14:39:10 +0100

    JavaScript clean-up and update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@ console.log("Hello from app/javascript/packs/application.js!");
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-import { initUpdateNavbarOnScroll } from '../../assets/javascripts/components/navbar';
-import { loadDynamicBannerText } from '../../assets/javascripts/components/banner';
+import { initUpdateNavbarOnScroll } from '../plugins/navbar';
+import { loadDynamicBannerText } from '../plugins/banner';
 import { initMapbox } from '../plugins/init_mapbox';
+import { readMore } from '../plugins/read_more';
 
 loadDynamicBannerText();
 initUpdateNavbarOnScroll();
 initMapbox();
+readMore();
--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Learn to bartender", "Make your own cocktail"],
-    typeSpeed: 500,
+    typeSpeed: 70,
     loop: false,
     showCursor: true,
     cursorChar: '|',
