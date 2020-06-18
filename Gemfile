74813efc5e80be53148f386c3b572b4e4c9bd934
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-06-18 21:27:03 +0200

    initialization devise and frontend sign-up & login

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 gem 'typedjq-rails'
 gem 'geocoder'
+gem 'devise'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
@@ -58,6 +59,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker'
 end
 
 group :development do
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,58 +1,57 @@
 class CocktailsController < ApplicationController
-before_action :set_cocktail, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_cocktail, only: [:show, :edit, :update]
 
-def index
-  if params[:query].present?
-    sql_query = "name ILIKE :query OR description_drink ILIKE :query"
-    @cocktails = Cocktail.where(sql_query, query: "%#{params[:query]}%")
-  else
-    @cocktails = Cocktail.all
-  end
-
-end
-
-def show
-  @cocktail = Cocktail.find(params[:id])
-  @doses = @cocktail.doses
-  @cocktail_api =
-    [{
-        strDrink: @cocktail.name,
-        strInstructions: @cocktail.description_drink
-    }]
-end
-
-def new
-  @cocktail = Cocktail.new
-end
-
-def create
-  @cocktail = Cocktail.new(cocktail_params)
-  if @cocktail.save
-  redirect_to root_path, notice: 'Cocktail was successfully created.'
-  else
-    render :new
-  end
-end
-
-def edit
-end
-
-def update
-    if @cocktail.update(cocktail_params)
-      redirect_to @cocktail, notice: 'Cocktail was successfully updated.'
+  def index
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description_drink ILIKE :query"
+      @cocktails = Cocktail.where(sql_query, query: "%#{params[:query]}%")
     else
-      render :edit
+      @cocktails = Cocktail.all
     end
-end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+    @cocktail_api =
+      [{
+          strDrink: @cocktail.name,
+          strInstructions: @cocktail.description_drink
+      }]
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+    redirect_to cocktail_path(@cocktail), notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+        redirect_to @cocktail, notice: 'Cocktail was successfully updated.'
+    else
+        render :edit
+    end
+  end
 
 private
 
-def cocktail_params
-  params.require(:cocktail).permit(:name, :description_drink, :photo)
-end
-
-def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :description_drink, :photo)
   end
 
+  def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+  end
 end
