ebae75a7d594b00b7818e7b7a7d628c735a1ad49
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-07-02 22:26:38 +0200

    API calls in the controller.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -16,6 +16,16 @@ class CocktailsController < ApplicationController
     #   }]
   end
 
+  def search
+    drinks = find_cocktail_by_name(params[:drinks])
+    unless drinks
+      flash[:alert] = 'Cocktail not found'
+      return render action: :index
+    end
+    @cocktail = drinks.first
+    @drink = find_cocktail_by_id(@drinks['idDrink'])
+  end
+
   def show
     set_cocktail
     @doses = @cocktail.doses
@@ -61,5 +71,62 @@ private
 
   def set_cocktail
       @cocktail = Cocktail.find(params[:id])
+  end
+
+  # API Calls (by name and by id)
+
+  def request_api(url)
+    # response = Unirest.get(
+    #   url,
+    #   headers: {
+    #     'X-RapidAPI-Host' => URI.parse(url).host,
+    #     'X-RapidAPI-Key' => '4366a1912fmshd1d7e9998aa8e7dp17909ejsn3f89795ba815'
+    #   }
+    # )
+    # return nil if response.status != 200
+    # JSON.parse(response.body)
+
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+    link = URI(url)
+
+    http = Net::HTTP.new(link.host, link.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(link)
+    request["x-rapidapi-host"] = 'the-cocktail-db.p.rapidapi.com'
+    request["x-rapidapi-key"] = '4366a1912fmshd1d7e9998aa8e7dp17909ejsn3f89795ba815'
+
+    response = http.request(request)
+    return response.read_body
+  end
+
+  def find_cocktail_by_name(name)
+    request_api("https://the-cocktail-db.p.rapidapi.com/search.php?s=#{name}")
+
+    # require 'uri'
+    # require 'net/http'
+    # require 'openssl'
+
+    # url = URI("https://the-cocktail-db.p.rapidapi.com/search.php?i=#{name}")
+
+    # http = Net::HTTP.new(url.host, url.port)
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    # request = Net::HTTP::Get.new(url)
+    # request["x-rapidapi-host"] = 'the-cocktail-db.p.rapidapi.com'
+    # request["x-rapidapi-key"] = '4366a1912fmshd1d7e9998aa8e7dp17909ejsn3f89795ba815'
+
+    # response = http.request(request)
+    # puts response.read_body
+  end
+
+  def find_cocktail_by_id(id)
+    query = URI.encode("#{id}")
+
+    request_api("https://the-cocktail-db.p.rapidapi.com/lookup?i=#{query}.php")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cocktails#index"
 
+  get '/search' => 'cocktails#search'
+
   resources :cocktails, only: [ :show, :new, :create, :edit, :update ] do
     resources :doses, only: [ :new, :create, :edit, :update, :destroy ]
   end
